f6d027ebbf4d67c2f3dad54c4b259e2e3afe598b
yoonhg84 <yoonhg2002@gmail.com>
2013-11-18 00:09:16 +0900

    Initial Commit

---
+++ ChopeLibrary/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ChopeLibrary
+//
+//  Created by Hyun Gook Yoon on 2013. 11. 18..
+//  Copyright (c) 2013년 Chope. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ChopeAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ChopeAppDelegate class]));
+    }
+}
---
+++ ChopeLibraryTests/ChopeLibraryTests.m
@@ -0,0 +1,34 @@
+//
+//  ChopeLibraryTests.m
+//  ChopeLibraryTests
+//
+//  Created by Hyun Gook Yoon on 2013. 11. 18..
+//  Copyright (c) 2013년 Chope. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ChopeLibraryTests : XCTestCase
+
+@end
+
+@implementation ChopeLibraryTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
